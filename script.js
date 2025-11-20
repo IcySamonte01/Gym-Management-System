e1190a7a916478c78c9d535e0bb2030d89f7647c
dajacjhonwilliam4-oss <dajacjhonwilliam4@gmail.com>
2025-11-20 19:55:17 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleSidebar = document.getElementById("toggleSidebar");
+const sidebar = document.querySelector(".sidebar");
+
+toggleSidebar.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+});
+
+
